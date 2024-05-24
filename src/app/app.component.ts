f373cb30815e13a50bb9e49fbc778e79d9c19841
Bogomaz Dmitry <bogomaz.dima2013@gmail.com>
2024-05-24 03:28:40 +0200

    added map and small point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import * as geojson from 'geojson';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Work tool';
+export class AppComponent implements OnInit {
+  private map: any;
 
-  isDarkTheme = false;
-  screenWidth = 80;
+  ngOnInit(): void {
+    this.initMap();
+  }
 
-  constructor() {}
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [44.815733, 20.495854],
+      zoom: 10,
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+    }).addTo(this.map);
+
+    const icon = L.icon({
+      iconSize: [38, 38],
+      iconUrl: 'assets/images/love.png',
+    });
+    const geojsonPoint: geojson.Point = {
+      coordinates: [20.495854, 44.815733],
+      type: 'Point',
+    };
+    const marker = L.geoJSON(geojsonPoint, {
+      pointToLayer: (point, latlon) => {
+        return L.marker(latlon, { icon: icon });
+      },
+    });
+    //Add popup message
+    marker.bindPopup('Я верю в твои идеи и сильно люблю!');
+    marker.addTo(this.map);
+  }
+
+  private openPopup(layer: any, feature: any): void {
+    const popupContent = `
+      <strong>Country:</strong> ${feature.properties.name}<br>
+      <form>
+        <label for="info">Info:</label>
+        <textarea id="info" name="info"></textarea><br>
+        <button type="button" onclick="saveData()">Save</button>
+      </form>
+    `;
+    layer.bindPopup(popupContent).openPopup();
+  }
 }
